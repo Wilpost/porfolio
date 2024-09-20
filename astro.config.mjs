762a656707f99249309cb71eb68222ee0935c266
Wilpost <wilyerqueiporiera@gmail.com>
2024-09-20 16:02:56 -0400

    Modificación de la configuración de astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
-import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
+import { defineConfig } from "astro/config"
+import tailwind from "@astrojs/tailwind"
+import react from "@astrojs/react"
 
-import react from '@astrojs/react'
+import vercel from "@astrojs/vercel/serverless"
 
 // https://astro.build/config
 export default defineConfig({
+  output: "hybrid",
+
   integrations: [tailwind(), react()],
-  output: 'hybrid'
+  adapter: vercel(),
 })
